7c7a6d0f91c154a442fa5094547baf5b7a1251db
potatoken777 <122539617+potatoken777@users.noreply.github.com>
2023-01-23 08:58:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
-input.onGesture(Gesture.TiltLeft, function () {
-	
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showString("A", 5)
+basic.showString("B", 5)
+basic.showString("C", 5)
+basic.showString("D", 5)
+    }
+    letter = randint(1, 4)
+    if (letter == 1) {
+        basic.showString("A")
+    } else if (letter == 2) {
+        basic.showString("B")
+    } else if (letter == 3) {
+        basic.showString("C")
+    } else {
+        basic.showString("D")
+    }
 })
-input.onButtonPressed(Button.AB, function () {
-    for (let index = 0; index < 200; index++) {
-        basic.showLeds(`
+let letter = 0
+let question = 0
+basic.forever(function () {
+    basic.showLeds(`
             . # # # .
             # . . . #
             . . # # .
             . . . . .
             . . # . .
-            `)
-        basic.pause(100)
-        basic.clearScreen()
-        basic.pause(100)
-    }
+            `, 10)
+basic.pause(100)
+    basic.clearScreen()
 })
